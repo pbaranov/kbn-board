bbb837aa8f888a6e003b927490ec8d6800d9b4fd
Petr Baranov <petr@petr-baranovs-macbook.local>
2011-03-16 22:57:15 +0200

    model initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KbnBoard::Application.config.secret_token = 'd6ddc296bd0da96649086568c92dd41a2ce8c025df3620444b53a550706cd8e70d8054a9e53671c172d5deb402c61c9b4ce60a1b16b3211f3cafd492452fb14f'
